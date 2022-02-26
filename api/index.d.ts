ba71ae33befd45a32b2c128a800e7e14940fcdaf
Lily Scott <git@suchipi.tv>
2022-02-25 17:41:43 -0700

    Update API

--- api/index.d.ts
+++ api/index.d.ts
@@ -3,7 +3,7 @@
  * the specified `rootDir`. If `rootDir` isn't present, it defaults to
  * `process.cwd()`.
  */
-export default function glomp(rootDir?: string): Glomp;
+export default function glomp(): Glomp;
 /**
  * An object that traverses the filesystem looking for files that
  * match a user-defined set of rules. Use the methods on the `Glomp`
@@ -12,100 +12,96 @@ export default function glomp(rootDir?: string): Glomp;
  * those filtering rules.
  */
 export declare class Glomp {
-    rules: Array<(absolutePath: string, isDir: boolean) => boolean>;
-    rootDir: string;
+    rules: Array<(info: {
+        rootDir: string;
+        absolutePath: string;
+        isDir: boolean;
+    }) => boolean>;
     /**
-     * Create a new glomp instance that traverses and resolves paths relative to
-     * the specified `rootDir`.
-     */
-    constructor(rootDir: string);
-    /**
-     * A utility function that resolves `somePath` relative to this Glomp's
-     * rootDir, and strips a trailing slash off of the end of said path, if
-     * present.
-     *
-     * If `somePath` is absolute, it not be resolved relative to rootDir, but the
-     * trailing slash will still be stripped off.
-     */
-    resolvePath(somePath: string): string;
-    /**
-     * Add a rule to this Glomp specifying that only files within
-     * the specified directory should be included in the results
-     * from `findMatches` or `findMatchesSync`.
+     * Return a new Glomp with all the rules of this Glomp plus a new rule
+     * specifying that only files within the specified directory should be
+     * included in the results from `findMatches` or `findMatchesSync`.
      *
      * If `someDir` isn't an absolute path, it will be resolved into
      * an absolute path by using the Glomp's rootDir property as
      * the directory to resolve from. If this isn't desired, pass
      * in an absolute path instead.
      */
-    withinDir(someDir: string): this;
+    withinDir(someDir: string): Glomp;
     /**
-     * Add a rule to this Glomp specifying that files within
-     * the specified directory should _NOT_ be included in the results
-     * from `findMatches` or `findMatchesSync`.
+     * Return a new Glomp with all the rules of this Glomp plus a new rule
+     * specifying that files within the specified directory should _NOT_ be
+     * included in the results from `findMatches` or `findMatchesSync`.
      *
      * If `someDir` isn't an absolute path, it will be resolved into
      * an absolute path by using the Glomp's rootDir property as
      * the directory to resolve from. If this isn't desired, pass
      * in an absolute path instead.
      */
-    excludeDir(someDir: string): this;
+    excludeDir(someDir: string): Glomp;
     /**
-     * Add a rule to this Glomp specifying that only files with the
-     * specified filetype extension should be included in the results
-     * from `findMatches` or `findMatchesSync`.
-     */
-    withExtension(extension: string): this;
-    /**
-     * Add a rule to this Glomp specifying that files with the specified filetype
-     * extension should _NOT_ be included in the results from `findMatches` or
-     * `findMatchesSync`.
-     */
-    excludeExtension(extension: string): this;
-    /**
-     * Add a rule to this Glomp specifying that only files which are
-     * **immediate children** of the specified directory should be included in
-     * the results from `findMatches` or `findMatchesSync`. This means that files
-     * that exist in subdirectories of the specified directotry will _NOT_ be
-     * included. If you want to include all files *including* those in
-     * subdirectories, use `withinDir` instead.
-     *
-     * If `someDir` isn't an absolute path, it will be resolved into
-     * an absolute path by using the Glomp's rootDir property as
-     * the directory to resolve from. If this isn't desired, pass
-     * in an absolute path instead.
-     */
-    immediateChildrenOfDir(someDir: string): this;
-    /**
-     * Add a rule to this Glomp specifying that files which are
-     * **immediate children** of the specified directory should _NOT_ be
-     * included in the results from `findMatches` or `findMatchesSync`. However,
-     * files that exist in subdirectories of the specified directory _will_ be
-     * included. If you want to exclude all of the contents of a directory,
-     * including files in subdirectories of that directory, use `excludeDir`
-     * instead.
-     *
-     * If `someDir` isn't an absolute path, it will be resolved into
-     * an absolute path by using the Glomp's rootDir property as
-     * the directory to resolve from. If this isn't desired, pass
-     * in an absolute path instead.
-     */
-    excludeImmediateChildrenOfDir(someDir: string): this;
-    /**
-     * Add a custom rule to this Glomp that determines whether paths should
+     * Return a new Glomp with all the rules of this Glomp plus a new rule
+     * specifying that only files with the specified filetype extension should
      * be included in the results from `findMatches` or `findMatchesSync`.
+     */
+    withExtension(extension: string): Glomp;
+    /**
+     * Return a new Glomp with all the rules of this Glomp plus a new rule
+     * specifying that files with the specified filetype extension should _NOT_
+     * be included in the results from `findMatches` or `findMatchesSync`.
+     */
+    excludeExtension(extension: string): Glomp;
+    /**
+     * Return a new Glomp with all the rules of this Glomp plus a new rule
+     * specifying that only files which are **immediate children** of the
+     * specified directory should be included in the results from `findMatches`
+     * or `findMatchesSync`.
+     *
+     * This means that files that exist in subdirectories of the specified
+     * directory will _NOT_ be included. If you want to include all files
+     * *including* those in subdirectories, use `withinDir` instead.
+     *
+     * If `someDir` isn't an absolute path, it will be resolved into
+     * an absolute path by using the Glomp's rootDir property as
+     * the directory to resolve from. If this isn't desired, pass
+     * in an absolute path instead.
+     */
+    immediateChildrenOfDir(someDir: string): Glomp;
+    /**
+     * Return a new Glomp with all the rules of this Glomp plus a new rule
+     * specifying that files which are **immediate children** of the specified
+     * directory should _NOT_ be included in the results from `findMatches` or
+     * `findMatchesSync`. However, files that exist in subdirectories of the
+     * specified directory _will_ be included. If you want to exclude all of
+     * the contents of a directory, including files in subdirectories of that
+     * directory, use `excludeDir` instead.
+     *
+     * If `someDir` isn't an absolute path, it will be resolved into
+     * an absolute path by using the Glomp's rootDir property as
+     * the directory to resolve from. If this isn't desired, pass
+     * in an absolute path instead.
+     */
+    excludeImmediateChildrenOfDir(someDir: string): Glomp;
+    /**
+     * Return a new Glomp with all the rules of this Glomp plus a new custom
+     * rule that determines whether paths should be included in the results from
+     * `findMatches` or `findMatchesSync`.
      *
      * Rules are functions with this signature:
      * ```ts
-     * function myRule(absolutePath: string, isDir: boolean): boolean;
+     * function myRule(info: {
+     *   absolutePath: string,
+     *   isDir: boolean,
+     *   rootDir: string
+     * }): boolean;
      * ```
      *
      * The rule function will be called repeatedly with absolute paths referring
      * to files and folders on disk, and should return either true or false.
      *
-     * When `isDir` is false, that means that Glomp is asking you:
+     * When `info.isDir` is false, that means that Glomp is asking you:
      *
-     * "`absolutePath` refers to a file. Should this file be included in the
+     * "`info.absolutePath` refers to a file. Should this file be included in the
      * results from `findMatches` or `findMatchesSync`?""
      *
      * - If the answer is no, return `false`, indicating that this file should
@@ -114,9 +110,9 @@ export declare class Glomp {
      * - If the answer is yes, return `true`, indicating that this file _should_
      *   be included in the output.
      *
-     * When `isDir` is true, that means that Glomp is asking you:
+     * When `info.isDir` is true, that means that Glomp is asking you:
      *
-     * "`absolutePath` refers to a directory. Could there be any files in this
+     * "`info.absolutePath` refers to a directory. Could there be any files in this
      * directory that should be included in the results from `findMatches` or
      * `findMatchesSync`?"
      *
@@ -127,21 +123,81 @@ export declare class Glomp {
      *   return `false`, indicating that there is no need to search through the
      *   contents of that firectory.
      *
+     * `info.rootDir` contains the absolute path to the root directory passed to
+     * `findMatches` or `findMatchesSync`.
+     *
      * Internally, all the rule-defining methods on a Glomp use the same
      * rule mechanism as `customRule`.
      */
-    customRule(rule: (absolutePath: string, isDir: boolean) => boolean): this;
+    customRule(rule: (info: {
+        rootDir: string;
+        absolutePath: string;
+        isDir: boolean;
+    }) => boolean): Glomp;
     /**
-     * Asynchronously scan through folders in the filesystem, finding files
+     * Create a new Glomp by combining the rules in this Glomp with the rules in
+     * another Glomp.
+     *
+     * The new Glomp will only match files that satisfy the rules in both Glomps.
+     *
+     * @param other The other Glomp to combine with.
+     */
+    and(other: Glomp): Glomp;
+    /**
+     * Create a new Glomp by combining the rules in this Glomp with the rules in
+     * another Glomp.
+     *
+     * The new Glomp will only match files that satisfy the rules in this Glomp
+     * and DO NOT satisfy the rules in the other Glomp.
+     *
+     * This is the same as .and(other.inverse()), but using it makes your code
+     * easier to read, because the "inverse" part would otherwise be at the
+     * bottom of a long chain.
+     *
+     * @param other The other Glomp to combine with.
+     */
+    andNot(other: Glomp): Glomp;
+    /**
+     * Create a new Glomp by combining the rules in this Glomp with the rules in
+     * another Glomp.
+     *
+     * The new Glomp will match files that satisfy the rules in either this Glomp or the other one.
+     *
+     * @param other The other Glomp to combine with.
+     */
+    or(other: Glomp): Glomp;
+    /**
+     * Create a new Glomp by inverting all the rules in this glomp.
+     *
+     * The new Glomp will exclude files that this Glomp matches.
+     *
+     * @param other The other Glomp to combine with.
+     */
+    inverse(): Glomp;
+    /**
+     * Return a new Glomp with all the rules of this Glomp plus a new rule
+     * specifying that the entire absolute path to a file must match the provided
+     * regular expression.
+     */
+    withAbsolutePathMatchingRegExp(regexp: RegExp): Glomp;
+    /**
+     * Return a new Glomp with all the rules of this Glomp plus a new rule
+     * specifying that filenames must match the provided regular expression.
+     */
+    withNameMatchingRegExp(regexp: RegExp): Glomp;
+    /**
+     * Asynchronously scan through the specified folder, finding files
      * that match the rules that have been defined on this Glomp instance.
      *
-     * To define rules on this Glomp instance, use any of these methods:
+     * To define rules, use any of these methods:
      * - `withinDir`
      * - `excludeDir`
      * - `withExtension`
      * - `excludeExtension`
      * - `immediateChildrenOfDir`
      * - `excludeImmediateChildrenOfDir`
+     * - `withAbsolutePathMatchingRegExp`
+     * - `withNameMatchingRegExp`
      * - `customRule`
      *
      * After the relevant directories have all been searched, the Promise
@@ -165,7 +221,7 @@ export declare class Glomp {
      * The concurrency value defaults to the number of CPUs your system has
      * minus one.
      */
-    findMatches({ concurrency, }?: {
+    findMatches(rootDir: string, { concurrency, }?: {
         /**
          * How many concurrent fs-scanning Promises are allowed at one time.
          *
@@ -174,10 +230,10 @@ export declare class Glomp {
         concurrency?: number;
     }): Promise<Array<string>>;
     /**
-     * Synchronously scans through folders in the filesystem, finding files
+     * Synchronously scans through the specified folder, finding files
      * that match the rules that have been defined on this Glomp instance.
      *
-     * To define rules on this Glomp instance, use any of these methods:
+     * To define rules, use any of these methods:
      * - `withinDir`
      * - `excludeDir`
      * - `withExtension`
@@ -193,5 +249,5 @@ export declare class Glomp {
      * Note that ONLY PATHS TO FILES WILL BE RETURNED, _NOT_ PATHS TO
      * FOLDERS! This is an intentional design decision of `glomp`.
      */
-    findMatchesSync(): Array<string>;
+    findMatchesSync(rootDir: string): Array<string>;
 }
